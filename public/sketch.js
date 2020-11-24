261bd97b84523278aedb3ab6a079865a939fea13
chcapu <chc.caputo@gmail.com>
2020-11-24 06:53:55 +0100

    connection printed on both server and client

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,12 @@
 let socket = io(); //4.load socket.io (that is in the index.html) for the client side;
 
+//print the connection on the client side
+socket.on("connect", newConnection); //=when the "connect" message is received, execute the function newConnection;
+function newConnection() {
+  console.log("your id: " + socket.id);
+}
+
 function preload(){
-  // put preload code here
 }
 
 function setup() {
@@ -10,5 +15,4 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
 }
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ let server = app.listen(port); //1.
 app.use(express.static("public")); //2.send the public folder to the client
 let io = socket(server); //3.
 
+//print the connection on the server side
 io.on("connection", newConnection); //=on the event "connection", execute the function newConnection
 function newConnection(socket) {  //function executed everytime a new client connection is created
   console.log("new connection: " + socket.client.id); //to read the connection ID of every client on the server;
