fa4c84b5b690c3ed7286f188d49cdc7096a7d76d
chcapu <chc.caputo@gmail.com>
2020-11-24 06:45:29 +0100

    newConnection print on the server side

---
+++ public/sketch.js
@@ -0,0 +1,14 @@
+let socket = io(); //4.load socket.io (that is in the index.html) for the client side;
+
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background("purple");
+}
+
+function draw() {
+  // put drawing code here
+}
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("node is running");
+
+//1.create a local server (express-app-port-server): use express to setup a server app running on server side;
+let express = require("express"); //1.
+let socket = require("socket.io"); //3.create a web socket (socket-io): use socket to enable the server to communicate with the client;
+let app = express(); //1.
+let port = 3000; //1.
+let server = app.listen(port); //1.
+app.use(express.static("public")); //2.send the public folder to the client
+let io = socket(server); //3.
+
+io.on("connection", newConnection); //=on the event "connection", execute the function newConnection
+function newConnection(socket) {  //function executed everytime a new client connection is created
+  console.log("new connection: " + socket.client.id); //to read the connection ID of every client on the server;
+}
