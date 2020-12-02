c844fa37325d86f867195a5b273fa83c18670983
chcapu <chc.caputo@gmail.com>
2020-12-02 07:02:20 +0100

    delivery

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,7 @@
 let socket = io(); //4.load socket.io (that is in the index.html) for the client side;
-let myColor = "white";
+
+//--SKETCH FATTO IN AULA RICREATO--
+//let myColor = "white";
 
 let easy;
 let lovey;
@@ -7,26 +9,37 @@ let mad;
 let moody;
 let sick;
 
+let myMood;
+let message;
+
+
 //print the connection on the client side
 socket.on("connect", newConnection); //=when the "connect" message is received, execute the function newConnection;
 function newConnection() {
   console.log("your id: " + socket.id);
 }
 
+//--SKETCH FATTO IN AULA RICREATO--
 //set brush color
-socket.on("color", setColor); //=when the "color" message is received from the server, execute setColor;
-function setColor(assignedColor) {  //(data from the message)
-  myColor = assignedColor;
+  // socket.on("color", setColor); //=when the "color" message is received from the server, execute setColor;
+  // function setColor(assignedColor) {  //(data from the message)
+  //   myColor = assignedColor;
+  // }
+
+//set starting mood
+socket.on("mood", setMood); //=when the "color" message is received from the server, execute setColor;
+function setMood(assignedMood) {  //(data from the message)
+  myMood = loadImage("./assets/emojis/" + assignedMood + ".png");
 }
 
-
+//--SKETCH FATTO IN AULA RICREATO--
 //broadcast what other clients are drawing
-socket.on("mouseBroadcast", drawOtherMouse); //=when another client broadcasts the mouse data, execute drawOtherMouse;
-function drawOtherMouse(data) { //data is the message of the mouse position sent from another client to the server and then broadcasted to the other clients;
-  fill(data.color);
-  noStroke();
-  ellipse(data.x, data.y, 20);
-}
+  // socket.on("mouseBroadcast", drawOtherMouse); //=when another client broadcasts the mouse data, execute drawOtherMouse;
+  // function drawOtherMouse(data) { //data is the message of the mouse position sent from another client to the server and then broadcasted to the other clients;
+  //   fill(data.color);
+  //   noStroke();
+  //   ellipse(data.x, data.y, 20);
+  // }
 
 
 function preload() {
@@ -37,22 +50,125 @@ function preload() {
   sick = loadImage("./assets/emojis/sick.png");
 }
 
-function setup() {
-  createCanvas(windowWidth,windowHeight)
-  background("AliceBlue");
+//broadcast other clients drawings and make them all hearts
+socket.on("mouseBroadcast", drawOtherMouse);
+function drawOtherMouse(data) {
+ imageMode(CENTER);
+ image(lovey, data.x, data.y, 29, 29);
 }
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background("AliceBlue");
+
+  push();
+  let intro = "Express your feelings, get nothing but love back!"
+  fill(0);
+  textSize(24);
+  textFont("Jost");
+  textAlign(CENTER);
+  text(intro,width/2,60);
+  pop();
+
+  push();
+  let choice = "How do you feel today? [c]:🤙🏻 [v]:💜 [b]:😡 [n]:🌧 [m]:🦠";
+  textSize(18);
+  textFont("Jost");
+  textAlign(CENTER);
+  text(choice,width/2,height-90);
+  pop();
+
+  push();
+  let instruction = "CLICK AND DRAW, SOMEONE MIGHT REPLY...";
+  textSize(11);
+  fill("DodgerBlue");
+  textFont("Jost");
+  textAlign(CENTER);
+  text(instruction,width/2,height-50);
+  pop();
+}
+
 
 function draw() {
 }
 
-function mouseMoved() { //=when the mouse is moved...
-  fill(myColor);
-  noStroke();
-  ellipse(mouseX, mouseY, 20); //...a sketch happens on the client side...
-  let message = {
+
+//--SKETCH FATTO IN AULA RICREATO--
+// function mouseMoved() { //=when the mouse is moved...
+//   fill(myColor);
+//   noStroke();
+//   ellipse(mouseX, mouseY, 20); //...a sketch happens on the client side...
+//   let message = {
+//     x: mouseX,
+//     y: mouseY,
+//     color: myColor,
+//   }; //..and a message..
+//   socket.emit("mouse", message) //..is sent to the server, through my instance of socket.io..
+// }
+
+function mousePressed() {
+  imageMode(CENTER);
+  image(myMood,mouseX,mouseY,29,29);
+  message = {
     x: mouseX,
     y: mouseY,
-    color: myColor,
-  }; //..and a message..
-  socket.emit("mouse", message) //..is sent to the server, through my instance of socket.io..
+  };
+  socket.emit("mouse", message);
+}
+
+function mouseDragged() {
+  imageMode(CENTER);
+  image(myMood,mouseX,mouseY,29,29);
+  message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit("mouse", message);
+}
+
+function keyPressed() {
+  console.log(key);
+  if(key === "c"){
+    myMood = easy;
+  } if (key === "v") {
+    myMood = lovey;
+  } if (key === "b") {
+    myMood = mad;
+  } if (key === "n") {
+    myMood = moody;
+  } if (key === "m") {
+    myMood = sick;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background("AliceBlue");
+
+  push();
+  let intro = "Express your feelings, get nothing but love back!"
+  fill(0);
+  textSize(24);
+  textFont("Jost");
+  textAlign(CENTER);
+  text(intro,width/2,60);
+  pop();
+
+  push();
+  let choice = "How do you feel today? [c]:🤙🏻 [v]:💜 [b]:😡 [n]:🌧 [m]:🦠";
+  textSize(18);
+  textFont("Jost");
+  textAlign(CENTER);
+  text(choice,width/2,height-90);
+  pop();
+
+  push();
+  let instruction = "CLICK AND DRAW, SOMEONE MIGHT REPLY...";
+  textSize(11);
+  fill("DodgerBlue");
+  textFont("Jost");
+  textAlign(CENTER);
+  text(instruction,width/2,height-50);
+  pop();
 }
--- server.js
+++ server.js
@@ -17,23 +17,60 @@ function newConnection(socket) {  //function executed everytime a new client con
 
 //////////////////////
 
+
+//--SKETCH FATTO IN AULA RICREATO--
 //client color
-  let clientColor = getRandomColor();
-  socket.emit("color", clientColor); //tell to the specific client a message titled "color", which tells the color that the function getRandomColor generated;
+  //let clientColor = getRandomColor();
+  //socket.emit("color", clientColor); //tell to the specific client a message titled "color", which tells the color that the function getRandomColor generated;
+
+//client mood
+let clientMood = getRandomMood();
+socket.emit("mood", clientMood); //tell to the specific client a message titled "color", which tells the color that the function getRandomColor generated;
+
+
+//--SKETCH FATTO IN AULA RICREATO--
+//broadcast the data received by the client to all the others
+  // socket.on("mouse", mouseMessage); //=when you get the message "mouse", execute mouseMessage;
+  // function mouseMessage(dataReceived) { //the mouseMessage function takes the received data and..
+  //   console.log(socket.client.id, dataReceived); //..prints it on the server side with the id of the client that sent the data.. (when the client moves the mouse, the server prints the x and y positions; so the client sends a message to the server);
+  //   socket.broadcast.emit("mouseBroadcast", dataReceived); //..and broadcasts to all the other connections, excluding the one that sent it.
+  // }
 
 //broadcast the data received by the client to all the others
-  socket.on("mouse", mouseMessage); //=when you get the message "mouse", execute mouseMessage;
-  function mouseMessage(dataReceived) { //the mouseMessage function takes the received data and..
-    console.log(socket.client.id, dataReceived); //..prints it on the server side with the id of the client that sent the data.. (when the client moves the mouse, the server prints the x and y positions; so the client sends a message to the server);
-    socket.broadcast.emit("mouseBroadcast", dataReceived); //..and broadcasts to all the other connections, excluding the one that sent it.
-  }
+socket.on("mouse", mouseMessage);
+function mouseMessage(dataReceived) {
+  console.log(socket.client.id, dataReceived);
+  socket.broadcast.emit("mouseBroadcast", dataReceived);
 }
 
-function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for(var i=0; i<6; i++) {
-    color += letters[Math.floor(Math.random()*16)];
-  }
-  return color;
 }
+
+function getRandomMood() {
+// tentativo n.1 :fail
+  // let easy;
+  // let lovey;
+  // let mad;
+  // let moody;
+  // let sick;
+  //
+  // let possibleMoods = [easy, lovey, mad, moody, sick];
+  // let mood = possibleMoods[Math.floor(Math.random()*possibleMoods.length)];
+  // console.log("my random mood is: " + mood);
+  // return mood;
+
+//tentativo n.2
+  let possibleMoods = ['easy', 'lovey', 'mad', 'moody', 'sick'];
+  let mood = possibleMoods[Math.floor(Math.random()*possibleMoods.length)];
+  console.log("my random mood is: " + mood);
+  return mood;
+}
+
+//--SKETCH FATTO IN AULA RICREATO--
+//function getRandomColor() {
+  // let letters = "0123456789ABCDEF";
+  // let color = "#";
+  // for(var i=0; i<6; i++) {
+  //   color += letters[Math.floor(Math.random()*16)];
+  // }
+  // return color;
+//}
